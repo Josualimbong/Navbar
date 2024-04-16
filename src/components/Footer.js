9fdbd4c057b14a68b79f96f16ea56d5d7b2087a4
Josua Limbong <josualimkbong21@gmail.com>
2024-04-17 01:21:49 +0900

    navbar

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-orange-300 text-black py-4 font-bold">
+      <div className="container mx-auto text-center font-mono">
+        <p>&copy; 2024 Josua Limbong. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
